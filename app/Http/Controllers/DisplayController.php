737402ee1ef62127e3d2b31cc5ff9f0a6d72ebd6
kinanbosyri <77687080+kinanbosyri@users.noreply.github.com>
2022-12-03 17:59:14 +0200

    Added userfilesreserved, usergroups, to DisplayController

--- app/Http/Controllers/DisplayController.php
+++ app/Http/Controllers/DisplayController.php
@@ -11,12 +11,22 @@ class DisplayController extends Controller
 {
     public function userfiles(Request $request)
     {
-        return UserFileResource::collection($request->user()->files);
+        return UserFileResource::collection($request->user()->owned_files);
     }
 
     public function groupfiles($group)
     {
         $main = Group::find($group);
         return GroupFileResource::collection($main->files);
+    }
+
+    public function usergroups(Request $request)
+    {
+        return UserFileResource::collection($request->user()->groups);
+    }
+
+    public function userfilesreserved(Request $request)
+    {
+        return UserFileResource::collection($request->user()->reserved_files);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -31,28 +31,42 @@ Route::middleware(['acid','logger'])->post('login',[AuthController::class,'login
 Route::middleware('auth:sanctum')->group(function (){
 
     Route::middleware(['acid','logger'])->group(function(){
+
+        Route::get('logout',[AuthController::class,'logout']);
+
+        /* File Add And Delete Controller Routes */
         Route::delete('/delete-file',[FileAddAndDeleteController::class,'deleteFile']);
         Route::delete('/delete-file-from-group/{group_id}',[FileAddAndDeleteController::class,'deleteFileFromGroup']);
-        Route::get('logout',[AuthController::class,'logout']);
         Route::post('/add-file',[FileAddAndDeleteController::class,'AddNewFileToGroup']);
         Route::post('/add-file-to-group/{group_id}',[FileAddAndDeleteController::class,'AddFileToGroup']);
+
+        /* Group Add And Delete Controller Routes */
         Route::post('/add-group',[GroupAddAndDeleteController::class,'creategroup']);
         Route::delete('/delete-group/{group}',[GroupAddAndDeleteController::class,'deletegroup']);
         Route::post('/groups/{group}/add-user',[GroupAddAndDeleteController::class,'addusers']);
         Route::delete('/groups/{group}/delete-user',[GroupAddAndDeleteController::class,'deleteuser']);
+
+        /* Display Controller Routes */ 
         Route::get('/user/files',[DisplayController::class,'userfiles']);
+        Route::get('/user/files/reserved',[DisplayController::class,'userfilesreserved']);
+        Route::get('/user/groups/',[DisplayController::class,'usergroups']);
         Route::get('/groups/{group}/files',[DisplayController::class,'groupfiles']);
+
+        /*File Operations Controller Routes*/ 
         Route::get('/reqd-file/{uuid}',[FileOperationsController::class,'readFile']);
         Route::post('/files/check-in',[FileOperationsController::class,'bulk_check_in']);
         Route::post('/files/{uuid}/check-in',[FileOperationsController::class,'Check_in']);
         Route::get('/files/{uuid}/download',[FileOperationsController::class,'download_file']);
         Route::post('/files/{uuid}/check-out',[FileOperationsController::class,'check_out']);
+
+        /* History Controller Routes */
         Route::get('/files/{id}/history',[HistoryController::class,'view_history']);
 
     });
 
     Route::middleware(['authorize.admin','acid','logger'])->group(function(){
 
+        /* Admin Controller Routes */
         Route::get('/admin/all-files',[AdminController::class,'view_all_files']);
         Route::get('/admin/all-groups',[AdminController::class,'view_all_groups']);
 
